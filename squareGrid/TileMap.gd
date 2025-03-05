245c09027f5f7d843563d540cd677bb474b400b1
GORTRONTHEDESTROYER <therealgortron@gmail.com>
2025-03-04 19:25:39 -0600

    iteration006
    for juju's plessure

--- squareGrid/TileMap.gd
+++ squareGrid/TileMap.gd
@@ -2,7 +2,7 @@ extends TileMap
 
 
 # Called when the node enters the scene tree for the first time.
-var gridSize = 11
+var gridSize = 30
 var Dic = {}
 
 func _ready():
@@ -10,19 +10,32 @@ func _ready():
 		for y in gridSize:
 			Dic[str(Vector2(x,y))] = {
 				"Type": "Field",
-				"Position": str(Vector2(x,y))
+				"Position": Vector2i(x,y)
 				
 			}
 			set_cell(0,Vector2(x,y), 0, Vector2i(0,0),0)
-	print(Dic)
 var turegate = false	
 
 
 func _on_play_pressed():
 	turegate = true  # Replace with 
-
+	
+func mousepressed():
+	var tile = local_to_map(get_viewport().get_mouse_position())
+	if (turegate == true ):	
+		if Input.is_action_pressed("LEFT_MOUSE_BUTTON"):
+			if Dic.has(str(tile)):
+				set_cell(0, tile, 2, Vector2i(0,0),0)
+				Dic[str(tile)] = {
+					"Type": "Forest",
+					"Position": tile
+				}
+		
+	
+	
 	
 func _process(delta):
+
 	var tile = local_to_map(get_viewport().get_mouse_position())
 	
 	for x in gridSize:
@@ -31,12 +44,13 @@ func _process(delta):
 	
 	if Dic.has(str(tile)):
 		set_cell(1, tile, 1, Vector2i(0,0),0)
-		print(Dic[str(tile)])
+		#print(Dic[str(tile)])
+		print(Dic[str(tile)]["Position"])
 		
-	if (turegate == true):	
-	
-		if Dic.has(str(tile)):
-			set_cell(0, tile, 2, Vector2i(0,0),0)
-			print(Dic[str(tile)])
+
+	mousepressed()
+		
+		
+			
 		
 	
