e3c330448c1522b244ac9f181822e048e87b1aba
GORTRONTHEDESTROYER <therealgortron@gmail.com>
2025-03-08 23:57:01 -0600

    Iteration 014
    Units and play mode initialized, buttons moved to bottom of screen. Play mode no longer scans everysingle tile always, instead only scanning when mouse is pressed

--- squareGrid/scripts/TileGrid.gd
+++ squareGrid/scripts/TileGrid.gd
@@ -20,12 +20,10 @@ func _ready():
 				Tile.new(Constants.TileType.FIELD, Constants.Tribe.IMP, Vector2i(x, y))
 			)
 			set_cell(Layer.TILE, Vector2i(x, y), Constants.Asset.FIELD, Vector2i.ZERO, 0)
-			
-			
-	#set_cell(Layer.UNIT, Vector2i(0,0), Constants.Asset.WARRIOR, Vector2i.ZERO, 0)
 
 	tiles[0][0].unit = Unit.new(Constants.UnitType.WARRIOR, Constants.Player.ONE)
 	print(tiles[0][0].unit.type)
+	updateUnitLook(tiles[0][0])
 var prev_tile_pos: Vector2i = Vector2i(0, 0)
 
 func _process(_delta):
@@ -40,21 +38,103 @@ func _process(_delta):
 	if tile != null:
 		set_cell(2, tile_pos, 0, Vector2i(0,0),0)
 		prev_tile_pos = tile_pos
-		
+
 		if Input.is_action_pressed("LEFT_MOUSE_BUTTON")&&(mode == 0):
 			updateTile(tile)
-		elif tile.unit != null:
-			turnMode(tile)
-		for x in range(gridSize):
-			for y in range(gridSize):
-				updateLook(tiles[x][y])
-
-		#updateUnitLook()
-
+			updateLook(tile)
+		
+	if mode == 1:
+		turnMode(tile)
 
 var tile_type_bt = Constants.TileType.NONE
 var mode = 0
 
+#Buttons Moved to Bottom
+		
+func updateTile(tile: Tile):
+	tile.resourceLevel = resource_level_bt
+	tile.type = tile_type_bt
+	
+func updateLook(tile: Tile):
+	
+	match [tile.type, tile.resourceLevel]:
+		[Constants.TileType.FIELD, 0]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.FIELD, Vector2i.ZERO, 0)
+			erase_cell(Layer.RESOURCE, tile.position)
+		[Constants.TileType.FIELD, 1]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.FIELD, Vector2i.ZERO, 0)
+			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.FRUIT, Vector2i.ZERO, 0)
+		[Constants.TileType.FIELD, 2]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.FIELD, Vector2i.ZERO, 0)
+			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.CROP, Vector2i.ZERO, 0)
+		[Constants.TileType.FOREST, 1]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.FOREST, Vector2i.ZERO, 0)
+			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.ANIMAL, Vector2i.ZERO, 0)
+		[Constants.TileType.FOREST, _]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.FOREST, Vector2i.ZERO, 0)
+			erase_cell(Layer.RESOURCE, tile.position)
+		[Constants.TileType.MOUNTAIN, 2]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.MOUNTAIN, Vector2i.ZERO, 0)
+			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.METAL, Vector2i.ZERO, 0)
+		[Constants.TileType.MOUNTAIN, _]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.MOUNTAIN, Vector2i.ZERO, 0)
+			erase_cell(Layer.RESOURCE, tile.position)
+		[Constants.TileType.CLOUD, _]:
+			set_cell(Layer.TILE, tile.position, Constants.Asset.CLOUD, Vector2i.ZERO, 0)
+			erase_cell(Layer.RESOURCE, tile.position)
+		_:
+			pass
+
+
+func updateAllUnitLook():
+	for x in range(gridSize):
+			for y in range(gridSize):
+				updateUnitLook(tiles[x][y])
+	
+
+func updateUnitLook(tileW: Tile):
+	
+	if tileW.unit != null:
+		
+		match tileW.unit.type:
+			Constants.UnitType.WARRIOR:
+				set_cell(Layer.UNIT, tileW.position, Constants.Asset.WARRIOR, Vector2i.ZERO, 0)
+			_: 
+				pass
+	else:
+		erase_cell(Layer.UNIT, tileW.position)
+			
+			
+func tileCheck_Active():
+	for x in range(gridSize):
+		for y in range(gridSize):
+			if tiles[x][y].unit != null:
+				return [tiles[x][y].unit.active, tiles[x][y]] 
+	return [false]
+			
+func turnMode(tile: Tile):
+		
+	if Input.is_action_just_pressed("LEFT_MOUSE_BUTTON"):
+		
+		var tileFinder = tileCheck_Active()
+		
+		if tileFinder[0] == true && tile != null:
+			tileFinder[1].unit = null
+			tile.unit = Unit.new(Constants.UnitType.WARRIOR, Constants.Player.ONE)
+			tile.unit.active = not tile.unit.active
+			set_cell(Layer.UNIT, tile.position, Constants.Asset.WARRIOR, Vector2i.ZERO, 0)
+			print(tile.unit.active)
+			updateAllUnitLook()
+		
+		
+		if tile != null && tile.unit != null:
+
+			tile.unit.active = not tile.unit.active
+			
+			updateUnitLook(tile)
+			
+			
+#######################################################################
 func _on_mountain_button_toggled(toggled_on: bool):
 	if toggled_on:
 		tile_type_bt = Constants.TileType.MOUNTAIN
@@ -100,74 +180,5 @@ func _on_toggle_play_toggled(toggled_on: bool):
 		mode = 1
 	else:
 		mode = 0 
-		
-func updateTile(tile: Tile):
-	tile.resourceLevel = resource_level_bt
-	tile.type = tile_type_bt
-	
-func updateLook(tile: Tile):
-	
-	match [tile.type, tile.resourceLevel]:
-		[Constants.TileType.FIELD, 0]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.FIELD, Vector2i.ZERO, 0)
-			erase_cell(Layer.RESOURCE, tile.position)
-		[Constants.TileType.FIELD, 1]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.FIELD, Vector2i.ZERO, 0)
-			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.FRUIT, Vector2i.ZERO, 0)
-		[Constants.TileType.FIELD, 2]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.FIELD, Vector2i.ZERO, 0)
-			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.CROP, Vector2i.ZERO, 0)
-		[Constants.TileType.FOREST, 1]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.FOREST, Vector2i.ZERO, 0)
-			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.ANIMAL, Vector2i.ZERO, 0)
-		[Constants.TileType.FOREST, _]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.FOREST, Vector2i.ZERO, 0)
-			erase_cell(Layer.RESOURCE, tile.position)
-		[Constants.TileType.MOUNTAIN, 2]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.MOUNTAIN, Vector2i.ZERO, 0)
-			set_cell(Layer.RESOURCE, tile.position, Constants.Asset.METAL, Vector2i.ZERO, 0)
-		[Constants.TileType.MOUNTAIN, _]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.MOUNTAIN, Vector2i.ZERO, 0)
-			erase_cell(Layer.RESOURCE, tile.position)
-		[Constants.TileType.CLOUD, _]:
-			set_cell(Layer.TILE, tile.position, Constants.Asset.CLOUD, Vector2i.ZERO, 0)
-			erase_cell(Layer.RESOURCE, tile.position)
-		_:
-			pass
-
-	if tile.unit == null:
-		
-		erase_cell(Layer.UNIT, tile.position)
-		return
-		
-	match tile.unit.type:
-		Constants.UnitType.WARRIOR:
-			set_cell(Layer.UNIT, tile.position, Constants.Asset.WARRIOR, Vector2i.ZERO, 0)
-		_: 
-			pass
-func updateUnitLook(tileW: Tile):
-	
-	if tileW.unit != null:
-		
-		match tileW.unit.type:
-			Constants.UnitType.WARRIOR:
-				set_cell(Layer.UNIT, tileW.position, Constants.Asset.WARRIOR, Vector2i.ZERO, 0)
-			_: 
-				pass
-	#else:
-	#if tile.unit == null:
-		#erase_cell(Layer.UNIT, tile.position)
 			
-func turnMode(tile: Tile):
-	
-	if Input.is_action_just_pressed("LEFT_MOUSE_BUTTON"):
-		tile.unit.active = not tile.unit.active
-		print(tile.unit.active)
 		
-	
-
-
-
-
-
-
